e837baebb5c3186dfd86d530ce7f9124432fa8cb
0x1eef <0x1eef@hardenedbsd.org>
2026-05-27 05:54:29 -0300

    docs: add yardoc comments to null_lock.rb

--- mrblib/mruby-chan/null_lock.rb
+++ mrblib/mruby-chan/null_lock.rb
@@ -1,9 +1,32 @@
-module Chan
-  class NullLock
-    def self.lock; end
-    def self.lock_nonblock; end
-    def self.release; end
-    def self.close; end
-    def self.lockable?; true; end
-  end
+# frozen_string_literal: true
+
+##
+# {Chan::NullLock Chan::NullLock} is a no-op lock
+# that can be used instead of a file lock when
+# locking is not needed.
+class Chan::NullLock
+  ##
+  # @return [void]
+  #  This method is a no-op
+  def self.lock; end
+
+  ##
+  # @return [void]
+  #  This method is a no-op
+  def self.lock_nonblock; end
+
+  ##
+  # @return [void]
+  #  This method is a no-op
+  def self.release; end
+
+  ##
+  # @return [void]
+  #  This method is a no-op
+  def self.close; end
+
+  ##
+  # @return [true]
+  #  Always returns true
+  def self.lockable?; true; end
 end
