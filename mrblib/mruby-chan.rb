2fc1d50bab29da432549f72e3f20f8423b0ffb94
0x1eef <0x1eef@hardenedbsd.org>
2026-05-27 05:58:00 -0300

    fix: use Chan.tmpdir instead of Dir.tmpdir for mruby compat

--- mrblib/mruby-chan.rb
+++ mrblib/mruby-chan.rb
@@ -1,21 +1,19 @@
 # frozen_string_literal: true
 
-##
-# @author 0x1eef
-# @since 0.1.0
-# @example
-#   ch = xchan(Marshal)
-#   ch.send([1, 2, 3])
-#   ch.recv.pop # => 3
-#   ch.close
 module Chan
+  ##
+  # @return [String]
+  #  Returns a path suitable for temporary files
+  def self.tmpdir
+    ENV["TMPDIR"] || ENV["TMP"] || "/tmp"
+  end
 end
 
 module Kernel
   ##
   # @example
-  #   ch = xchan(Marshal)
-  #   ch.send([1,2,3])
+  #   ch = xchan(JSON)
+  #   ch.send([1, 2, 3])
   #   ch.recv.pop # => 3
   #   ch.close
   # @param [#dump, #load] serializer
@@ -25,7 +23,7 @@ module Kernel
   # @param [Symbol, Chan::NullLock, Chan::Lockf] lock
   #  The name of a lock (`:null` or `:file`), or a lock object
   # @return [Chan::Pipe]
-  def xchan(serializer, tmpdir: Dir.tmpdir, lock: :null)
+  def xchan(serializer, tmpdir: Chan.tmpdir, lock: :null)
     Chan::Pipe.new(serializer, tmpdir: tmpdir, lock: lock)
   end
 end
