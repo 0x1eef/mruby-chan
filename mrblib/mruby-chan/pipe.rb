f9e92e03162397758c5382674a06a230f0bf4e59
0x1eef <0x1eef@hardenedbsd.org>
2026-05-27 06:03:33 -0300

    docs: re-add yardoc to pipe.rb

--- mrblib/mruby-chan/pipe.rb
+++ mrblib/mruby-chan/pipe.rb
@@ -1,8 +1,29 @@
 # frozen_string_literal: true
 
+##
+# {Chan::Pipe Chan::Pipe} is a channel that uses
+# {IO.pipe} for InterProcess Communication. It
+# provides a send/recv interface with optional
+# file locking for synchronisation across processes.
 class Chan::Pipe
-  attr_reader :r, :w
+  ##
+  # @return [IO]
+  #  Returns the read end of the pipe
+  attr_reader :r
 
+  ##
+  # @return [IO]
+  #  Returns the write end of the pipe
+  attr_reader :w
+
+  ##
+  # @param [#dump, #load] serializer
+  #  An object that implements `dump` and `load`
+  # @param [String] tmpdir
+  #  Directory where temporary files can be stored
+  # @param [Symbol, Chan::NullLock, Chan::Lockf] lock
+  #  The name of a lock (`:null` or `:file`), or a lock object
+  # @return [Chan::Pipe]
   def initialize(serializer, tmpdir: Chan.tmpdir, lock: :null)
     @s = Chan.serializers[serializer]&.call || serializer
     @r, @w = IO.pipe
@@ -14,10 +35,18 @@ class Chan::Pipe
     @lock = init_lock(lock)
   end
 
+  ##
+  # @return [Boolean]
+  #  Returns true when the channel is closed
   def closed?
     @r.closed? && @w.closed?
   end
 
+  ##
+  # Closes the channel and removes temporary files
+  # @raise [IOError]
+  #  When the channel is already closed
+  # @return [void]
   def close
     @lock.lock
     raise IOError, "closed channel" if closed?
@@ -28,6 +57,17 @@ class Chan::Pipe
     raise
   end
 
+  ##
+  # @group Write methods
+
+  ##
+  # Performs a blocking write
+  # @param [Object] object
+  #  An object to serialise and send
+  # @raise [IOError]
+  #  When the channel is closed
+  # @return [Integer]
+  #  Returns the number of bytes written to the channel
   def send(object)
     send_nonblock(object)
   rescue Chan::WaitWritable
@@ -36,6 +76,16 @@ class Chan::Pipe
   end
   alias_method :write, :send
 
+  ##
+  # Performs a non-blocking write
+  # @param [Object] object
+  #  An object to serialise and send
+  # @raise [IOError]
+  #  When the channel is closed
+  # @raise [Chan::WaitWritable]
+  #  When a write to {#w} blocks
+  # @return [Integer]
+  #  Returns the number of bytes written to the channel
   def send_nonblock(object)
     @lock.lock_nonblock
     raise IOError, "closed channel" if closed?
@@ -52,6 +102,18 @@ class Chan::Pipe
   end
   alias_method :write_nonblock, :send_nonblock
 
+  ##
+  # @endgroup
+
+  ##
+  # @group Read methods
+
+  ##
+  # Performs a blocking read
+  # @raise [IOError]
+  #  When the channel is closed
+  # @return [Object]
+  #  Returns a deserialised object from the channel
   def recv
     recv_nonblock
   rescue Chan::WaitReadable
@@ -60,6 +122,14 @@ class Chan::Pipe
   end
   alias_method :read, :recv
 
+  ##
+  # Performs a non-blocking read
+  # @raise [IOError]
+  #  When the channel is closed
+  # @raise [Chan::WaitReadable]
+  #  When a read from {#r} blocks
+  # @return [Object]
+  #  Returns a deserialised object from the channel
   def recv_nonblock
     @lock.lock_nonblock
     raise IOError, "closed channel" if closed?
@@ -73,32 +143,73 @@ class Chan::Pipe
   end
   alias_method :read_nonblock, :recv_nonblock
 
+  ##
+  # @endgroup
+
+  ##
+  # @group Wait methods
+
+  ##
+  # Waits for the channel to become readable
+  # @param [Float, Integer, nil] timeout
+  #  The number of seconds to wait before timeout.
+  #  Waits indefinitely with no arguments
+  # @return [Chan::Pipe, nil]
+  #  Returns self when the channel is readable, otherwise returns nil
   def wait_readable(timeout = nil)
     @r.wait_readable(timeout) and self
   end
 
+  ##
+  # Waits for the channel to become writable
+  # @param [Float, Integer, nil] timeout
+  #  The number of seconds to wait before timeout.
+  #  Waits indefinitely with no arguments
+  # @return [Chan::Pipe, nil]
+  #  Returns self when the channel is writable, otherwise returns nil
   def wait_writable(timeout = nil)
     @w.wait_writable(timeout) and self
   end
 
+  ##
+  # @endgroup
+
+  ##
+  # @group Stat methods
+
+  ##
+  # @return [Boolean]
+  #  Returns true when the channel is empty
   def empty?
     return true if closed?
     size.zero?
   end
 
+  ##
+  # @return [Integer]
+  #  Returns the number of objects waiting to be read
   def size
     @bytes.size
   end
 
+  ##
+  # @return [Integer]
+  #  Returns the total number of bytes written to the channel
   def bytes_sent
     @counter.bytes_written
   end
   alias_method :bytes_written, :bytes_sent
 
+  ##
+  # @return [Integer]
+  #  Returns the total number of bytes read from the channel
   def bytes_received
     @counter.bytes_read
   end
   alias_method :bytes_read, :bytes_received
+
+  ##
+  # @endgroup
 
   private
 
