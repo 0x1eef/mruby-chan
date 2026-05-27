5d230ac0436f812a5155e8b5314d77da4297ae46
0x1eef <0x1eef@hardenedbsd.org>
2026-05-27 05:54:22 -0300

    docs: add yardoc comments to counter.rb

--- mrblib/mruby-chan/counter.rb
+++ mrblib/mruby-chan/counter.rb
@@ -1,52 +1,76 @@
-module Chan
-  class Counter
-    def initialize(path)
-      @io = File.open(path, File::RDWR | File::CREAT | File::TRUNC)
-      @io.binmode
-      @io.sync = true
-      write(@io, 0, 0)
-    end
+# frozen_string_literal: true
 
-    def bytes_written
-      _, bytes = read(@io)
-      bytes
-    end
+##
+# {Chan::Counter Chan::Counter} provides a counter
+# for the number of written and received bytes on a
+# given channel.
+class Chan::Counter
+  ##
+  # @param [String] path
+  #  Path to a file used to back the counter
+  # @return [Chan::Counter]
+  def initialize(path)
+    @io = File.open(path, File::RDWR | File::CREAT | File::TRUNC)
+    @io.binmode
+    @io.sync = true
+    write(@io, 0, 0)
+  end
 
-    def bytes_read
-      bytes, _ = read(@io)
-      bytes
-    end
+  ##
+  # @return [Integer]
+  #  Returns the number of bytes written to a channel
+  def bytes_written
+    _, bytes = read(@io)
+    bytes
+  end
 
-    def increment!(bytes_read: 0, bytes_written: 0)
-      bytes_in, bytes_out = read(@io)
-      bytes_in += bytes_read
-      bytes_out += bytes_written
-      write(@io, bytes_in, bytes_out)
-    end
+  ##
+  # @return [Integer]
+  #  Returns the number of bytes read from a channel
+  def bytes_read
+    bytes, _ = read(@io)
+    bytes
+  end
 
-    def close
-      @io.close
-    end
+  ##
+  # @param [Integer] bytes_read
+  #  Number of bytes read to increment the counter by
+  # @param [Integer] bytes_written
+  #  Number of bytes written to increment the counter by
+  # @return [void]
+  # @private
+  def increment!(bytes_read: 0, bytes_written: 0)
+    bytes_in, bytes_out = read(@io)
+    bytes_in += bytes_read
+    bytes_out += bytes_written
+    write(@io, bytes_in, bytes_out)
+  end
 
-    private
+  ##
+  # Close the counter
+  # @return [void]
+  def close
+    @io.close
+  end
 
-    def write(io, bytes_read, bytes_written)
-      io.rewind
-      io.truncate(0)
-      io.write(serialize(bytes_read, bytes_written))
-      io.rewind
-    end
+  private
 
-    def read(io)
-      deserialize(io.read).tap { io.rewind }
-    end
+  def write(io, bytes_read, bytes_written)
+    io.rewind
+    io.truncate(0)
+    io.write(serialize(bytes_read, bytes_written))
+    io.rewind
+  end
 
-    def serialize(bytes_read, bytes_written)
-      [bytes_read, bytes_written].pack("Q>Q>")
-    end
+  def read(io)
+    deserialize(io.read).tap { io.rewind }
+  end
 
-    def deserialize(payload)
-      payload.unpack("Q>Q>")
-    end
+  def serialize(bytes_read, bytes_written)
+    [bytes_read, bytes_written].pack("Q>Q>")
+  end
+
+  def deserialize(payload)
+    payload.unpack("Q>Q>")
   end
 end
